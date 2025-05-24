b587bb27bfff669acb5b37372f96aec17b9b8d5d
Rohans-7 <rohansonawane1818@gmail.com>
2025-05-24 14:26:45 +0530

    Redis caching

--- doctor-service/src/main/java/com/cs/doctorservice/DoctorServiceApplication.java
+++ doctor-service/src/main/java/com/cs/doctorservice/DoctorServiceApplication.java
@@ -3,11 +3,11 @@ package com.cs.doctorservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cache.annotation.EnableCaching;
+// import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCaching
+// @EnableCaching
 public class DoctorServiceApplication {
 
     public static void main(String[] args) {
